8dc6d5917848a55d71abd20178a966b0b2615a87
Cherkasik <ilyakopeysk@gmail.com>
2019-05-18 19:48:05 +0300

    something strange

--- func1.py
+++ func1.py
@@ -1,45 +1,56 @@
-from numpy import exp, array, random, dot, transpose
-from math import cos
+from numpy import exp, array, random, dot, transpose, zeros, mean, amax, cos, square
+
 class NN:
     def __init__(self):
+        random.seed(1)
         self.sigmoid = lambda x: 1/(1+exp(-x))
         self.function = lambda x: exp(x)/cos(x)
         self.sigmoidPrime = lambda x: x * (x - 1)
         self.training_inputs = []
         self.training_outputs = []
-        self.weights1 = random.randn(1, 3)
-        self.weights2 = random.randn(3, 1)
+        self.weights1 = 2*random.random((1, 3)) - 1
+        self.weights2 = 2*random.random((3, 1)) - 1
         self.error = 0
         self.adjustments = 0
+
     def generate_set(self, n):
+        now = -10
+        step = 20/n
         for i in range (n):
-            self.training_inputs.append([float(i+1)])
-            self.training_outputs.append([float(self.function(i+1))])
+            self.training_inputs.append([float(now)])
+            self.training_outputs.append([float(self.function(now))])
+            now += step
         self.training_inputs = array(self.training_inputs, dtype=float)
         self.training_outputs = array(self.training_outputs, dtype=float)
-    
-    def backward(self, o):
-        self.o_error = self.training_outputs - o
-        self.o_delta = self.o_error * self.sigmoidPrime(o)
-        self.z2_error = self.o_delta.dot(self.weights2.T)
-        self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
-        self.weights1 += self.training_inputs.T.dot(self.z2_delta)
-        self.weights2 += self.z2.T.dot(self.o_delta)
-        
-    def train(self):
-        o = self.predict(self.training_inputs)
-        self.backward(o)
+        self.training_inputs /= 10
+        self.maxi = amax(abs(self.training_outputs))
+        self.training_outputs /= amax(abs(self.training_outputs), axis=0)
 
-    def predict(self, input):
-        self.z = dot(input, self.weights1)
-        self.z2 = self.sigmoid(self.z)
-        self.z3 = dot(self.z2, self.weights2)
-        o = self.sigmoid(self.z3[0])
-        return o
+    def backward(self):
+        l2_error = self.training_outputs - self.layer2
+        l2_delta = l2_error*self.sigmoidPrime(self.layer2)
+        l1_error = l2_delta.dot(self.weights1)
+        l1_delta = dot(l1_error, self.sigmoidPrime(self.layer1))
+        self.weights2 += self.layer1.T.dot(l2_delta)
+        self.weights1 += self.training_inputs.T.dot(l1_delta)
+
+    def train(self):
+        self.predict(self.training_inputs)
+        self.backward()
+
+    def predict(self, input1): 
+        self.layer1 = self.sigmoid(dot(input1, self.weights1))
+        self.layer2 = self.sigmoid(dot(self.layer1, self.weights2))
+
+    def output_predict(self, input1):
+        self.predict(input1)
+        return self.layer2*self.maxi
 
 neural = NN()
-neural.generate_set(100)
-for i in range(150):
+neural.generate_set(3)
+for i in range(1310): #1310 perfect for 1
     neural.train()
-print("Predicted Output: " + str(neural.predict(1)))
-print("Actual Output:" + str(neural.training_outputs[0]))
+    #print ("Loss: \n" + str(mean(square(neural.training_outputs - neural.output_predict(neural.training_inputs)))))
+a = 1
+print("Predicted Output: " + str(neural.output_predict(a)))
+print("Actual Output:" + str(neural.function(a)))
